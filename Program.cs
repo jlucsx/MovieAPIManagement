50f208c9a476de555893c89f0da32e2b819c2b1f
Jose Adalberto Lucas Lima <jalucas.jall@gmail.com>
2022-07-28 02:26:27 -0300

    Functions for database consumption segregated and GET request to send complete list of movies on database to the client implemented - to segregate later

--- Program.cs
+++ Program.cs
@@ -1,43 +1,50 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using MovieAPI.Data;
+using MovieAPI.Models;
 
+var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
+builder.Services.AddDbContext<MovieContext>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+EnsureDatabaseIsCreated(app);
+PopulateDatabaseIfEmpty(app);
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
+app.MapGet("/", Handler);
 app.Run();
 
-record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+async ValueTask<List<Movie>> Handler()
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var dbContext = services.GetRequiredService<MovieContext>();
+    var completeListOfMovies = await dbContext.Movies.ToListAsync();
+    return completeListOfMovies;
 }
+void EnsureDatabaseIsCreated(WebApplication webApplication)
+{
+    using var scope = webApplication.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var moviesDatabaseContext = services.GetRequiredService<MovieContext>();
+    moviesDatabaseContext.Database.EnsureCreated();
+}
+void PopulateDatabaseIfEmpty(WebApplication webApplication)
+{
+    using var scope = webApplication.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var moviesDatabaseContext = services.GetRequiredService<MovieContext>();
+    var databaseSeeder = new DatabaseSeeding(moviesDatabaseContext);
+    databaseSeeder.SeedDatabaseWithSampleData();
+}    
+
+public partial class Program { }
