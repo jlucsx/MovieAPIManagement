d4ebd09b691bcbce3816b1b8753f24de7624e08d
Jose Adalberto Lucas Lima <jalucas.jall@gmail.com>
2022-07-28 14:29:02 -0300

    Files for testing the two endpoints (/api/movies and /api/movies/id) along with a record class that supports the test with data and helper functions

---
+++ MovieListModel.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+using MovieAPI.Data;
+using MovieAPI.Models;
+
+namespace APITests;
+
+record MovieListModel
+{
+    private readonly List<Movie> _moviesFromDatabaseSeeding = DatabaseSeeding.MoviesForDatabaseSeeding;
+    
+    private readonly JsonSerializerOptions _jsonSerializerOptions = new()
+    {
+        Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false
+    };
+    public string GetSeedMovieListInJsonFormat()
+    {
+        return JsonSerializer.Serialize(_moviesFromDatabaseSeeding, _jsonSerializerOptions);
+    }
+
+    public string GetIdSpecifiedSeedMovieInJsonFormat(long id)
+    {
+        return JsonSerializer.Serialize(
+            _moviesFromDatabaseSeeding.FirstOrDefault(movie => movie.Id == id),
+            _jsonSerializerOptions);
+    }
+}
