fbaf573e8e5faf7f6480b2d4f30f8a9e6c3a751f
Jose Adalberto Lucas Lima <jalucas.jall@gmail.com>
2022-07-29 22:45:17 -0300

    Swagger removal; Indentation fix; Response message change when conflict occurs: return just a string instead of a single-property object.

--- MovieAPI/Program.cs
+++ MovieAPI/Program.cs
@@ -5,15 +5,13 @@ using MovieAPI.Models;
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<MovieContext>();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 app.UseHttpsRedirection();
 
@@ -37,38 +35,37 @@ app.MapGet("/api/movies/{id:long}", (long id, MovieContext dbContext) =>
 
 app.MapPost("/api/movies/add", (MovieContext dbContext, HttpRequest httpRequest) =>
 {
-        if (!httpRequest.HasJsonContentType())
-            return Results.StatusCode((int)HttpStatusCode.UnsupportedMediaType);
-        Movie jsonContent;
-        try
-        {
-            jsonContent = TryToParseReceivedMovie(httpRequest);
-        }
-        catch (Exception)
-        {
-            return Results.BadRequest("Could not parse the body of the request according to the provided Content-Type.");
-        }
-        if (IsAnyPropertyWithIncorrectFormat(jsonContent))
-            return Results.BadRequest("Malformed request syntax");
-        var databaseOperationsHandler = new DatabaseOperations(dbContext);
-        bool isMovieAlreadyOnDatabase = databaseOperationsHandler.IsMovieAlreadyOnDatabase(dbContext, jsonContent).Result;
-        if (isMovieAlreadyOnDatabase) return Results.Conflict(
-            new {
-                Detail = "The movie already exists on the database"
-            });
-        long addedMovieId;
-        try
-        {
-            addedMovieId = databaseOperationsHandler.AddMovie(jsonContent).Result;
-        }
-        catch (Exception)
-        {
-            return Results.StatusCode((int)HttpStatusCode.InternalServerError);
-        }
+    if (!httpRequest.HasJsonContentType())
+        return Results.StatusCode((int)HttpStatusCode.UnsupportedMediaType);
+    Movie jsonContent;
+    try
+    {
+        jsonContent = TryToParseReceivedMovie(httpRequest);
+    }
+    catch (Exception)
+    {
+        return Results.BadRequest("Could not parse the body of the request according to the provided Content-Type.");
+    }
 
-        return Results.Created($"/api/movies/{addedMovieId}", null);
+    if (IsAnyPropertyWithIncorrectFormat(jsonContent))
+        return Results.BadRequest("Malformed request syntax");
+    var databaseOperationsHandler = new DatabaseOperations(dbContext);
+    bool isMovieAlreadyOnDatabase = databaseOperationsHandler.IsMovieAlreadyOnDatabase(dbContext, jsonContent).Result;
+    if (isMovieAlreadyOnDatabase)
+        return Results.Conflict("The movie already exists on the database");
+    long addedMovieId;
+    try
+    {
+        addedMovieId = databaseOperationsHandler.AddMovie(jsonContent).Result;
+    }
+    catch (Exception)
+    {
+        return Results.StatusCode((int)HttpStatusCode.InternalServerError);
+    }
+
+    return Results.Created($"/api/movies/{addedMovieId}", null);
 });
-    
+
 app.Run();
 
 void EnsureDatabaseIsCreated(WebApplication webApplication)
